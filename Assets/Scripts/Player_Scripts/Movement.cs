5261f454b31868ae7072686ec149bb158e4313bf
RickyB2016 <1007553@student.sae.edu.au>
2017-03-13 14:28:32 +1100

    Possession Updated, Rage Added, Scripts Updated, Material Updated

--- Assets/Scripts/Player_Scripts/Movement.cs
+++ Assets/Scripts/Player_Scripts/Movement.cs
@@ -19,4 +19,5 @@ public class Movement : MonoBehaviour {
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 	}
+   
 }
---
+++ Assets/Scripts/Player_Scripts/Scare.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scare : MonoBehaviour {
+
+    private Animator myAnimator; 
+    public Animator myanimation;
+
+	// Use this for initialization
+	void Start () {
+      
+		
+        myAnimator = GetComponent<Animator>();
+        myAnimator.enabled = false;
+        myanimation = GetComponent<Animator>();
+        myanimation.enabled = false;
+        Time.timeScale = 0.0f;
+	}
+	
+   
+	// Update is called once per frame
+	void Update () {
+         
+        if (Input.GetKeyDown("m"))
+        {
+            Application.Quit();
+        }
+        
+        if (Input.GetKeyDown ("q")) 
+        {   
+            myAnimator.enabled = true;
+           
+            myAnimator.Play("WallScare");
+            myanimation.enabled = true;
+            myanimation.Play("APPROACH");
+          print ("You pressed Q ");
+                    
+        }
+	}
+}
