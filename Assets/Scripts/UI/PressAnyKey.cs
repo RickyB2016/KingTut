9303eabf46b34b0845a13e2db2bc47d676eca7ae
RickyB2016 <1007553@student.sae.edu.au>
2017-04-11 14:59:33 +1000

    Added NInjaScreen, PressAnyKey Scene, plus scripts, art

---
+++ Assets/Scripts/UI/PressAnyKey.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PressAnyKey : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void PressedKey()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.anyKey)
+        {
+            Debug.Log("You pressed any key");
+            PressedKey();
+        }
+		
+	}
+}
