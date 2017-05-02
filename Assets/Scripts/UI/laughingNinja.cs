814857dd835567b76bfc34ca59e6fc89cc9f14b3
RickyB2016 <1007553@student.sae.edu.au>
2017-05-02 14:05:29 +1000

    NinjaScreen + Timer + Spawn Sound Added
    ....

---
+++ Assets/Scripts/UI/laughingNinja.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class laughingNinja : MonoBehaviour {
+
+
+	void Start ()
+	{ 
+		StartCoroutine("Wait");
+
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(4);
+
+		SceneManager.LoadScene("PressAnyKey");
+	}
+}
