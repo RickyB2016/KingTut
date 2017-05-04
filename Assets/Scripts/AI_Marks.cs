9c6df3bb72fdf532920b0d78d34318333d02ced7
jlahood97 <john.lahood3@hotmail.com>
2017-05-04 20:36:37 +1000

    Question mark on A.I

---
+++ Assets/Scripts/AI_Marks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Marks : MonoBehaviour {
+
+
+	public GameObject mark;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			mark.gameObject.SetActive (true);
+		} 
+
+	}
+
+}
+
