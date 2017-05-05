3418ebfe53f53aeaf18543f02313206dbc71cb5b
jlahood97 <john.lahood3@hotmail.com>
2017-05-05 18:58:08 +1000

    Exclamation mark when being attacked working :DDDD

---
+++ Assets/Scripts/AI_Marks2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Marks2 : MonoBehaviour {
+
+	public GameObject mark;
+	public GameObject mark2;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			mark.gameObject.SetActive (false);
+			mark2.gameObject.SetActive (true);
+		} 
+
+	}
+}
